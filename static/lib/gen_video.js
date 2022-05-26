8b1a9961ae82f1518f40264a791fc904063c50e4
Ferdinand Mol <ferdinandmol8@gmail.com>
2022-05-26 22:28:50 +0200

    Fixed backnav bug

--- static/lib/gen_video.js
+++ static/lib/gen_video.js
@@ -5,10 +5,16 @@ $(function() {
     const rootURL = window.location.protocol + '//' + window.location.host;
 
 
+    // NAVIGATING BY BUTTON TO PREVENT BACK NAV: ---------------------------------
+
+    document.getElementById('nav-button').addEventListener('click', () => {
+        window.location.replace(`${rootURL}/interface-training/${participant_id}`)
+    });
+    
+
     // UPDATING STAGE: ----------------------------------------------------------
 
-    postable_data = {id:participant_id}
-
+    var postable_data = {id: participant_id}
     fetch(`${rootURL}/update_stage/gen_video`, {
         method: 'PUT',
         headers: {
@@ -21,7 +27,7 @@ $(function() {
         if (data.error) {
             console.error('Error:', data.error);                
         } else {
-            console.log(data)
+            // console.log(data)
         }
     })
     .catch((error) => {
@@ -29,4 +35,15 @@ $(function() {
     });
 
 
+    // ENABLING CONTINUE: -------------------------------------------------------
+    /* Sadly, the aparat video player does not support any onStateChange listening,    
+        so I had to set a timer based on the video length instead*/
+
+    AI_timeout = setTimeout(() => { 
+        document.getElementById("continue-button-highlight").classList.add('hidden');
+    }, 108000);
+
+    document.getElementById("continue-button-highlight").addEventListener('click', () => {
+        document.getElementById("continue-button-highlight").classList.add('hidden');
+    });
 });
