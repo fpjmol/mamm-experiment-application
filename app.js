af5de98e0d1af51a3b76ac32305d9ed86843c143
Ferdinand Mol <ferdinandmol8@gmail.com>
2022-07-10 14:50:52 +0200

    Changed to .env config

--- app.js
+++ app.js
@@ -19,32 +19,9 @@ app.use(cors())
 
 const mysql = require('mysql2');
 
-// SETTINGS FOR localhost DB:
-
-// const db = mysql.createConnection({
-//     user: 'root',
-//     host: 'localhost',
-//     password: '31804495598',
-//     database: 'experimentdatadb'
-// })
-
-// SETTINGS FOR HEROKU DB:
-
-// const db = mysql.createConnection({
-//     user: 'emr0lxlhgjmxblcg',
-//     host: 'clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-//     password: 'tyma6eiq0w7i3fna',
-//     database: 'cg511cow0to6am6v'
-// })
-
 // SETTINGS FOR IRAN DB:
 
-const db = mysql.createConnection({
-    user: 'experiment',
-    host: 'localhost',
-    password: 'CrChb_VWA20gh09dlTGeO',
-    database: 'db_experiment'
-})
+const db = mysql.createConnection(process.env.DB_URL);
 
 
 
